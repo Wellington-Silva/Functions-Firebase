d84f343eb7bd290565f322501096aa4e535a23d8
Wellington-Silva <wellingtonsilva112000@gmail.com>
2025-05-02 15:54:40 -0300

    Rename

--- functions/index.mjs
+++ functions/index.mjs
@@ -1,6 +1,6 @@
 import { onRequest } from "firebase-functions/v2/https";
-import UserController from "./src/Controllers/UserController.mjs";
-import TransformationService from "./src/Services/TransformationService.mjs";
+import UserController from "./src/controllers/UserController.mjs";
+import TransformationService from "./src/services/TransformationService.mjs";
 
 export const listusers = onRequest(UserController.list);
 export const showuser = onRequest(UserController.show);
